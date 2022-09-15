de921395fafd011b09796df37929c8bc8b2e1787
dmytro-bieliei <dmbieliei@gmail.com>
2022-09-15 11:46:57 +0300

    add word counter function

---
+++ assets/index.js
@@ -0,0 +1,34 @@
+// Дана строка текста: 
+'Lorem ipsum dolor sit amet consectetur adipisicing elit Perferendis unde'
+
+
+
+// Функция которая принимает строку и возвращает количество слов в ней.
+
+const taskString = 'Lorem ipsum dolor sit amet consectetur adipisicing elit Perferendis unde';
+const taskString2 = 'Yellow, orange, green, blue'
+const taskString3 = ' Lorem ipsum dolor sit amet consectetur adipisicing elit Recusandae consequatur doloribus Laudantium a iusto necessitatibus totam Quas aut debitis Aperiam illum similique eos mollitia eius accusantium sint voluptatum nesciunt Nobis illum rerum iure nemo tenetur Corporis ad vero delectus cupiditate reiciendis nihil labore Voluptatum quod similique'
+
+function wordCounter(string) {
+  const wordsArr = string.split(' ');
+  console.log(wordsArr);
+  const arrayLenght = wordsArr.length;
+  return arrayLenght + ' words in this string';
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Функция, которая получает строку и возвращает размер самого большого слова в ней
